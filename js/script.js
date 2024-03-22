1171fef4fac8c64748f927f4a97120a4fcf84ce0
tatangsundara <adityascout6@gmail.com>
2024-03-22 22:57:24 +0700

    fix css and image

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-// Toggle class active
+// Toggle class active Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Menu di klik
 document.querySelector("#Menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const menu = document.querySelector("#menu");
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const mn = document.querySelector("#menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!Menu.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!Mn.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!Sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
